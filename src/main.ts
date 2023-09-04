66aabe62c8a3180a0eb3d1f5053a16a0bb80f396
silvideastora <silvideastora@gmail.com>
2023-09-04 13:48:29 -0600

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,8 @@ import {ValidationPipe} from "@nestjs/common";
 const port = process.env.PORT || 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-      cors: false
-  });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(
       new ValidationPipe({
         transform: true,
