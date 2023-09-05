90ed358cb4d3289ba4147acba64a52b806d24de4
silvideastora <silvideastora@gmail.com>
2023-09-04 18:29:59 -0600

    manejo de error de login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {HttpException, HttpStatus, Injectable, UnauthorizedException} from '@nestjs/common';
 import {UsersService} from "../users/users.service";
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
@@ -12,9 +12,18 @@ export class AuthService {
 
     async signIn(email: string, pass: string): Promise<any> {
         const user = await this.usersService.findByEmail(email);
+        if (!user) {
+            throw new HttpException(
+                'Comprueba usuario y contraseña',
+                HttpStatus.BAD_REQUEST,
+            );
+        }
         console.log(pass, user.password)
         if (!bcrypt.compareSync(pass, user.password)) {
-            throw new UnauthorizedException();
+            throw new HttpException(
+                'Comprueba usuario y contraseña',
+                HttpStatus.BAD_REQUEST,
+            );
         }
         const payload = { email: user.email };
         return {
